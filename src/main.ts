efb99ef4650f74ca7da4f0712ce8d8be82433c7a
samank8121 <saman.k8121@gmail.com>
2025-09-17 14:34:04 +0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { PrismaExceptionFilter } from './filter/PrismaExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalFilters(new PrismaExceptionFilter());
-
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
